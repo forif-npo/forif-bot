8ecb15e496c687698f8e555f623498914835be6d
hwanji03 <bluekenn68@gmail.com>
2024-07-12 14:02:30 +0900

    forif-bot

--- model/database.py
+++ model/database.py
@@ -1,47 +1,45 @@
 import pymysql
 
-def db_connect():
-    global connection
 
-    connection = pymysql.connect(
-        host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
-        port=3306,
-        user='root',
-        passwd='Kangbh98!',
-        db='test',
-        charset='utf8'
-    )
+connection = pymysql.connect(
+    host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
+    port=3306,
+    user='root',
+    passwd='Kangbh98!',
+    db='test',
+    charset='utf8'
+)
 
 
 def get_study_list():
-    connection = db_connect()
+    cursor = connection.cursor()
 
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = '''
-        SELECT study_name, study_goal, study_web_url
-        FROM tb_study
-        WHERE act_year = 2024
-        AND act_semester = 2;
-        '''
+    sql = '''
+    SELECT study_name, study_goal, study_web_url
+    FROM tb_study
+    WHERE act_year = 2024
+    AND act_semester = 2;
+    '''
 
-        cursor.execute(sql)
-        result = cursor.fetchall()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
 
     return result
 
 
 def get_recent_notice():
-    connection = db_connect()
+    cursor = connection.cursor()
 
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = '''
-        SELECT notice_name, notice_date
+    sql = '''
+        SELECT notice_title, notice_date
         FROM tb_notice
         LIMIT 5;
         '''
 
-        cursor.execute(sql)
-        result = cursor.fetchall()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
 
     return result
 
