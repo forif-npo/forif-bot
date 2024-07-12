800282ae299bf4cd137404da64d2dd43a1d89594
hwanji03 <bluekenn68@gmail.com>
2024-07-12 12:48:55 +0900

    db_connect_auto

--- model/database.py
+++ model/database.py
@@ -2,14 +2,16 @@ import pymysql
 
 
 def get_study_list():
-    with pymysql.connect(
+    connection = pymysql.connect(
         host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
         port=3306,
         user='root',
         passwd='Kangbh98!',
         db='test',
         charset='utf8'
-    ) as connection:
+    )
+
+    try:
         cursor = connection.cursor(pymysql.cursors.DictCursor)
 
         sql = '''
@@ -24,25 +26,33 @@ def get_study_list():
 
         return result
 
+    finally:
+        connection.close()
+
 
 def get_recent_notice():
-    with pymysql.connect(
-            host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
-            port=3306,
-            user='root',
-            passwd='Kangbh98!',
-            db='test',
-            charset='utf8'
-    ) as connection:
+    connection = pymysql.connect(
+        host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
+        port=3306,
+        user='root',
+        passwd='Kangbh98!',
+        db='test',
+        charset='utf8'
+    )
+
+    try:
         cursor = connection.cursor(pymysql.cursors.DictCursor)
 
         sql = '''
-        SELECT notice_title, notice_date
-        FROM tb_notice
-        LIMIT 5;
-        '''
+            SELECT notice_title, notice_date
+            FROM tb_notice
+            LIMIT 5;
+            '''
 
         cursor.execute(sql)
         result = cursor.fetchall()
 
         return result
+
+    finally:
+        connection.close()
