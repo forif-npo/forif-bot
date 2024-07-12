ce1d101dad38d2bf8c58530aeec6a78c889ba2e1
hwanji03 <bluekenn68@gmail.com>
2024-07-12 12:25:00 +0900

    db_connection_auto

--- model/database.py
+++ model/database.py
@@ -1,47 +1,48 @@
 import pymysql
 
-connection = pymysql.connect(
-    host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
-    port=3306,
-    user='root',
-    passwd='Kangbh98!',
-    db='test',
-    charset='utf8'
-)
-
 
 def get_study_list():
-    try:
-        with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-            sql = '''
-            SELECT study_name, study_goal, study_web_url
-            FROM tb_study
-            WHERE act_year = 2024
-            AND act_semester = 2;
-            '''
+    with pymysql.connect(
+        host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
+        port=3306,
+        user='root',
+        passwd='Kangbh98!',
+        db='test',
+        charset='utf8'
+    ) as connection:
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
 
-            cursor.execute(sql)
-            connection.commit()
-            result = cursor.fetchall()
+        sql = '''
+        SELECT study_name, study_goal, study_web_url
+        FROM tb_study
+        WHERE act_year = 2024
+        AND act_semester = 2;
+        '''
 
-    finally:
-        connection.close()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
         return result
 
 
 def get_recent_notice():
-    try:
-        with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-            sql = '''
-               SELECT notice_title, notice_date
-               FROM tb_notice
-               LIMIT 5;
-               '''
+    with pymysql.connect(
+            host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
+            port=3306,
+            user='root',
+            passwd='Kangbh98!',
+            db='test',
+            charset='utf8'
+    ) as connection:
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
 
-            cursor.execute(sql)
-            connection.commit()
-            result = cursor.fetchall()
+        sql = '''
+        SELECT notice_title, notice_date
+        FROM tb_notice
+        LIMIT 5;
+        '''
 
-    finally:
-        connection.close()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
         return result
