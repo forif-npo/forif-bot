f2fbc76f082de4deeb8b01c811570f4cdc3c2ea8
hwanji03 <bluekenn68@gmail.com>
2024-07-19 11:44:46 +0900

    forif-bot

--- model/database.py
+++ model/database.py
@@ -1,18 +1,30 @@
 import pymysql
 
-connection = pymysql.connect(
-    host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
-    port=3306,
-    user='root',
-    passwd='Kangbh98!',
-    db='dev',
-    charset='utf8'
-)
+
+def run_query(sql):
+    connection = pymysql.connect(
+        host='forifdb.c1800c86ily5.ap-northeast-2.rds.amazonaws.com',
+        port=3306,
+        user='root',
+        passwd='Kangbh98!',
+        db='test',
+        charset='utf8'
+    )
+
+    try:
+        with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+        connection.commit()
+
+        return result
+
+    finally:
+        connection.close()
 
 
 def get_study_list():
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-
     sql = '''
     SELECT study_name, explanation, web_url
     FROM tb_study
@@ -20,26 +32,15 @@ def get_study_list():
     AND act_semester = 2;
     '''
 
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    cursor.close()
-
-    return result
+    return run_query(sql)
 
 
 def get_recent_notice():
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-
     sql = '''
-        SELECT title, created_at
-        FROM tb_post
-        WHERE post_type = '공지사항'
-        LIMIT 5;
-        '''
+    SELECT title, created_at
+    FROM tb_post
+    WHERE post_type = '공지사항'
+    LIMIT 5;
+    '''
 
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    cursor.close()
-
-    return result
-
+    return run_query(sql)
